76f9ae1dfe54ac0e5a58ac5aab5e8fc9db04a0d3
Lucas Massa <lmm@ic.ufal.br>
2022-09-25 22:52:22 -0300

    Removing about and contact

--- recipes/views.py
+++ recipes/views.py
@@ -3,12 +3,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return HttpResponse("contato")
-
-
-def sobre(request):
-    return HttpResponse("sobre")
+    return render(request, 'recipes/home.html', context = {
+        'name': 'Lucas Massa'
+    })
