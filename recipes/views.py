80753d7293eeebc27b0a4f5912956108eceb8dc8
Lucas Massa <lmm@ic.ufal.br>
2022-10-12 14:32:57 -0300

    Creating recipe-view

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context = {
         'name': 'Lucas Massa'
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context = {
+        'name': 'Lucas Massa'
+    })
